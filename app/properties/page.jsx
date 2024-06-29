d55cd46ecf7d8b0fc1063fd59edbb0f1ac306833
Martin Spassov <mspassov@uwo.ca>
2024-06-29 11:17:57 -0400

    home page completed

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
 
 const Properties = () => {
-  return <div>Properties</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties == null || properties.length == 0 ? (
+          <p>No properties currently available</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((curr) => (
+              <PropertyCard key={curr._id} property={curr} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Properties;
