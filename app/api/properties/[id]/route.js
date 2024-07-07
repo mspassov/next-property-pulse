ca0998742bc922efe2b668469d69393bed92bf94
Martin Spassov <mspassov@uwo.ca>
2024-07-07 19:00:38 -0400

    Added edit functionality

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -68,3 +68,70 @@ export const DELETE = async (request, {params}) =>{
         return new Response('Something went wrong', {status: 500})
     }
 }
+
+//PUT /api/properties/:id - updated an edited property, given an ID
+export const PUT = async (request, {params}) =>{
+    try {
+        await connectDB();
+        const sessionUser = await getSessionUser();
+
+        const { id } = params;
+        
+        const {userID} = sessionUser;
+        console.log(userID);
+
+        //Get the form data, using built in methods
+        const formData = await request.formData();
+
+        //Get the amenities
+        const amenities = formData.getAll('amenities');
+
+        //Get current property
+        const currProperty = await Property.findById(id);
+
+        if(!currProperty){
+            return new Response('Property does not exist', {status: 404});
+        }
+
+        //Verify ownership
+        if(userID !== currProperty.owner.toString()){
+            return new Response('Cannot access property', {status: 401})
+        }
+
+        //Create property object
+        const propertyData = {
+            type: formData.get('type'),
+            name: formData.get('name'),
+            description: formData.get('description'),
+            location: {
+                street: formData.get('location.street'),
+                city: formData.get('location.city'),
+                state: formData.get('location.state'),
+                zipcode: formData.get('location.zipcode'),
+            },
+            beds: formData.get('beds'),
+            baths: formData.get('baths'),
+            square_feet: formData.get('square_feet'),
+            amenities,
+            rates: {
+                weekly: formData.get('rates.weekly'),
+                monthly: formData.get('rates.monthly'),
+                nightly: formData.get('rates.nightly.'),
+            },
+            seller_info: {
+                name: formData.get('seller_info.name'),
+                email: formData.get('seller_info.email'),
+                phone: formData.get('seller_info.phone'),
+            },
+            owner: userID,
+        };
+
+        //Update the property in MongoDB
+        const updProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+        return new Response(JSON.stringify(updProperty), {status: 200});
+        
+    } catch (error) {
+        return new Response('Failed to update property', {status: 500})   
+    }
+}
--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import EditForm from '@/components/EditForm'
 
 const EditPage = () => {
   return (
-    <div>EditPage</div>
-  )
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <EditForm />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default EditPage
