0c632205194f98b35aeb4ac0932d64cd3863dae7
Martin Spassov <mspassov@uwo.ca>
2024-07-28 09:24:29 -0400

    Changed how session is called- needed for deployment

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -5,6 +5,10 @@ export const getSessionUser = async () =>{
     //Get the user that submitted the request
     const session = await getServerSession(authOptions);
 
+    if(!session || !session.user){
+        return null;
+    }
+
     return{
         user: session.user,
         userID: session.user.id
